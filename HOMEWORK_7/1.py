b5a783740d4c193efe70ee00cb1d06da683005b6
Mariya_Lazareva <lazareva.m1989@yandex.ru>
2023-02-22 01:13:44 +0300

    Homework 7

--- HOMEWORK_7/1.py
+++ HOMEWORK_7/1.py
@@ -16,3 +16,29 @@ sentense = [r.split('-') for r in input().split()]
 letters = ['о', 'а', 'и', 'е', 'ё', 'ы', 'э', 'ю', 'я', 'у']
 print(words(sentense, letters))
 
+# alp = "аеёиоуыэюя"
+# word_sug = input().split()
+# vowel_letters = [sum([True for j in word if j.lower() in alp]) for word in word_sug]
+
+# if all(vowel_letters) and len(set(vowel_letters)) == 1:
+#     print("Парам пам-пам")
+# else:
+#     print("Пам парам")
+
+# 2 вариант
+
+# def vowels_counter(S):
+#     vowels = list('а е ё и о у ы э ю я'.split())
+#     vowels_counter = list()
+#     for i in S:
+#         counter = 0
+#         for ch in i:
+#             if ch in vowels:
+#                 counter += 1
+#         vowels_counter.append(counter)
+#     return vowels_counter
+
+# # пара-ра-рам рам-пам-папам па-ра-па-дам
+# s = list("ff ff ff".split())  # можно заменить на input().split()
+
+# print("Парам пам-пам" if len(set(r := vowels_counter(s))) == 1 and r[0] != 0 else "Пам парам")
